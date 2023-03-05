e807b753898d92a69437f3db09f26222c5e18912
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-05 15:21:57 -0300

    feat: Add ActionListener for Threads

--- src/view/Tela.java
+++ src/view/Tela.java
@@ -5,14 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.ThreadCorrida;
+
 import javax.swing.JSeparator;
-import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
 public class Tela extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textFieldVenc;
 	private JTextField textFieldPerd;
@@ -50,17 +59,13 @@ public class Tela extends JFrame {
 		separator.setBounds(50, 130, 800, 2);
 		contentPane.add(separator);
 		
-		JLabel carroA = new JLabel("CARRO 1");
+		JLabel carroA = new JLabel("CARRO A");
 		carroA.setBounds(50, 60, 70, 15);
 		contentPane.add(carroA);
 		
-		JLabel carroB = new JLabel("CARRO 2");
+		JLabel carroB = new JLabel("CARRO B");
 		carroB.setBounds(50, 210, 70, 15);
 		contentPane.add(carroB);
-		
-		JButton btnCorrer = new JButton("Correr!");
-		btnCorrer.setBounds(50, 320, 90, 25);
-		contentPane.add(btnCorrer);
 		
 		JLabel lblVencedor = new JLabel("Vencedor:");
 		lblVencedor.setBounds(660, 290, 70, 15);
@@ -83,5 +88,16 @@ public class Tela extends JFrame {
 		textFieldPerd.setColumns(10);
 		textFieldPerd.setBounds(765, 345, 90, 20);
 		contentPane.add(textFieldPerd);
+		
+		JButton btnCorrer = new JButton("Correr!");
+		btnCorrer.setBounds(50, 320, 90, 25);
+		btnCorrer.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Thread threadCorrida = new ThreadCorrida(btnCorrer, carroA, carroB, textFieldVenc, textFieldPerd);
+				threadCorrida.start();
+			}
+		});
+		contentPane.add(btnCorrer);
 	}
 }
